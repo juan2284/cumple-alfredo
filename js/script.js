8f6bad110f63e8da4a5a4ba7248fc27ee5191f8a
Juan <juanitogye@gmail.com>
2024-09-17 02:43:55 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("load", () => {
+    const pantallaCarga = document.querySelector(".carga");
+    pantallaCarga.classList.add("d-none");
+  });
+});
